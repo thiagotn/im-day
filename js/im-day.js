333a81fea3d9cbf39fe3bad1d38b7a9521eb6524
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2017-05-16 12:40:53 -0300

    Adicionando Cezar e Bruna a fila de resolvedores

--- js/im-day.js
+++ js/im-day.js
@@ -3,7 +3,7 @@ var nextDaysToShow = 9; // if 10 = yesterday (1) + next days (10)
 var weekendImg = '<img src="img/WhatsApp.png" alt="WhatsApp" height="32" width="32">';
 
 $(document).ready(function(){
-	var users = ["Veiga", "Glauber", "Bruno", "Caio", "Gustavo", "Eduardo", "Thiago", "Maicon", "Cezar"];
+	var users = ["Veiga", "Glauber", "Caio", "Gustavo", "Eduardo", "Thiago", "Maicon", "Cezar"];
 	var count = 0;
 	var actualUser = 0;
 	var style = 'warning';
