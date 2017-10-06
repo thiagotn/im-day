c3ff220412e5d9a734bc466d74aec53a158cbef9
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2017-10-06 12:05:04 -0300

    Ajuste de ordem

--- js/im-day.js
+++ js/im-day.js
@@ -3,7 +3,7 @@ var nextDaysToShow = 9; // if 10 = yesterday (1) + next days (10)
 var weekendImg = '<img src="img/WhatsApp.png" alt="WhatsApp" height="32" width="32">';
 
 $(document).ready(function(){
-	var users = ["Veiga", "Cezar", "Glauber", "Caio", "Gustavo", "Thiago"];
+	var users = ["Cezar", "Glauber", "Caio", "Gustavo", "Thiago", "Veiga"];
 	var count = 0;
 	var actualUser = 0;
 	var style = 'warning';
